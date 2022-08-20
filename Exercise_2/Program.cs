e2b93b923e99b4854baf47b7f0c30fea11955382
Aleksey <a_zhuravlyov@bk.ru>
2022-08-20 12:58:20 +0300

    Finished Ex 2

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 
+*/
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+
+        while (num > 0)
+        {
+            sum = sum + num % 10;
+
+            num /= 10;
+        }
+        Console.WriteLine($"{sum}");
+    }
+}
+
